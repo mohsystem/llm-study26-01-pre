4002fd95117f03be16a8c15eba7045c7a47fecbd
moh <m.alalem@ubs-tech.com>
2026-03-15 00:26:43 +0200

    Initial commit

---
+++ src/test/java/com/um/springbootprojstructure/SpringBootProjStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.um.springbootprojstructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
